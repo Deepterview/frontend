6175a511c15549f72b3d5922035d76e876f04958
OnlyDev321 <onlydev.321@gmail.com>
2026-04-06 12:03:13 +0900

    fix SignIn-page(App.tsx and main.tsx)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Landing from "./pages/Landing";
 import SignIn from "./pages/SignIn";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="/signin" element={<SignIn />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
